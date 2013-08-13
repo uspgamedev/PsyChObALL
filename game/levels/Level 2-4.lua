0e62852ddf315027d70fbc8b3f73f06a228bae7e
Rilifon <pudimdefrutas@gmail.com>
2013-08-12 23:41:54 -0300

    2-3 mais fase pro boss 2
    NOME DAHORAS

---
+++ game/levels/Level 2-4.lua
@@ -0,0 +1,32 @@
+title = 'II - There and Back Again'
+chapter = 'Part 4 - Violent Love with Friends'
+
+function run()
+	doNow ( function( timer )
+		for i = 1, 8 do
+			local t = imagebody:new{ coloreffect = sincityeffect, image = graphics.newImage 'resources/warn.png', scale = .3 }
+			_G.enemy.__init(t)
+			t:register()
+		end
+		text:new { text = "BOSS INCOMING", font = getFont(40), position = vector:new{ -100, -30 }, speed = vector:new{v,v} }:register()
+	end )
+	wait(5)
+	enemy 'bossTwo'
+	wait(15)
+
+	registerTimer {
+		timelimit = .5,
+		funcToCall = function ( timer )
+			if not next(enemies.bossOne.bodies) then
+				text:new {
+					text = 'No turning back',
+					speed = vector:new{v, v},
+					size = 40,
+					position = vector:new{0,0},
+					handleDelete = function () lives = lives + 1 reloadStory 'Test Level' end
+				}:register()
+				timer:remove()
+			end
+		end
+	}
+end
