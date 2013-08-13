0c843a6f90403b11e5b2873b7ef70a34dd15e63e
Rilifon <pudimdefrutas@gmail.com>
2013-08-12 22:00:56 -0300

    2-3 half-way

--- game/levels/Level 2-3.lua
+++ game/levels/Level 2-3.lua
@@ -10,8 +10,53 @@ function run()
 	local grey = 'grayball'
 	local super = 'superball'
 
+	local f = formation {
+		type = 'horizontal',
+		from = 'left',
+		movetorwards = 'center',
+		setspeedto = vector:new{0, 0},
+		distance = 'distribute'
+	}
+
+	local verticalt = formation {
+		type = 'vertical',
+		from = 'top'
+	}
+
+	local verticalb = formation {
+		type = 'vertical',
+		from = 'bottom'
+	}
+
+	local horizontall = formation {
+		type = 'horizontal',
+		from = 'left'
+	}
+
+	local horizontalr = formation {
+		type = 'horizontal',
+		from = 'right'
+	}
+
+	wait(2)
+	for i = 1, 8 do
+		wait(.8)
+		f.from = 'left'
+		f.setspeedto.x = 360
+		enemy(simple, i, f)
+		f.from = 'right'
+		f.setspeedto.x = -360
+		enemy(divide1, i, f)
+	end
+	wait(4)
+	enemy(super, 1, { position = vector:new{width/2, -30}, speed = vector:new{v, v}, life = 120, size = 80}, simple, {width/2, -30}, 8)
+	wait(9)
+	enemy(super, 1, { position = vector:new{width/2, -30}, speed = vector:new{v, v}, life = 60, size = 50}, simple, {width/2, -30}, 12)
+	enemy(super, 1, { position = vector:new{width/2, height+30}, speed = vector:new{v, v}, life = 60, size = 50}, simple, {width/2, height+30}, 12)
+	wait(13)
+	enemy(super, 1, { position = vector:new{width/2, height+30}, speed = vector:new{v, v}, life = 120, size = 80}, divide1, {width/2, height+30}, 18)
 	wait(3)
-	enemy(super, 1, { position = {width/2, -30}, speed = {v, v}}, simple, {width/2, -30}, 10)
-
-
+	enemy(simple, 7, horizontalr)
+	wait(3)
+	enemy(simple, 7, horizontall)
 end
