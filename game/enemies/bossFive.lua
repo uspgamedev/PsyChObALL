1e5e66d9a7cf273cb2b6ccde4d81938768462a14
Yan Couto <yancouto@gmail.com>
2014-01-16 19:43:32 -0200

    Eyeball Image! Let's see where this goes.

--- game/enemies/bossFive.lua
+++ game/enemies/bossFive.lua
@@ -1,13 +1,13 @@
 local abs, random = math.abs, math.random
 
 bossFive = Body:new {
-	size = 100,
-	irisSize = 45,
+	size = 125,
+	irisSize = 55,
 	spriteBatch = false,
 	ord = 8,
 	variance = 4,
 	maxHealth = 100,
-	shader = Base.circleShader,
+	--shader = Base.circleShader,
 	__type = 'bossFive'
 }
 Body.makeClass(bossFive)
@@ -18,8 +18,8 @@ function bossFive:__init()
 	self.irisPosition = Vector:new{0, 0}
 	self.pupilSize = VarTimer:new{}
 	local funkShrink
-	local funcGrow = function(timer) timer:setAndGo(23, 26, 1 + random()) timer.alsoCall = funcShrink end
-	funcShrink = function(timer) timer:setAndGo(26, 23, 1 + random()) timer.alsoCall = funcGrow end
+	local funcGrow = function(timer) timer:setAndGo(27, 32, 1 + random()) timer.alsoCall = funcShrink end
+	funcShrink = function(timer) timer:setAndGo(32, 27, 1 + random()) timer.alsoCall = funcGrow end
 	funcGrow(self.pupilSize)
 	self.health = bossFive.maxHealth
 end
@@ -44,7 +44,7 @@ function bossFive:update( dt )
 	self:bounceInScreen()
 	self.irisPosition:set(psycho.position):sub(self.position):div(7)
 	local l = self.irisPosition:length()
-	if l > 25 then self.irisPosition:normalize():mult(25) end
+	if l > 33 then self.irisPosition:normalize():mult(33) end
 
 	for _, v in pairs(Shot.bodies) do
 		if self:collidesWith(v) then
@@ -64,15 +64,18 @@ function bossFive:update( dt )
 	end
 end
 
+local eyeballImage = graphics.newImage 'resources/eyeball.png'
 function bossFive:draw()
 	graphics.setColor(255, 255, 255)
-	graphics.circle('fill', self.position[1], self.position[2], self.size)
+	graphics.draw(eyeballImage, self.position[1] - self.size, self.position[2] - self.size)
 
 	--drawing pupil
+	graphics.setPixelEffect(Base.circleShader)
 	graphics.setColor(ColorManager.getComposedColor(self.variance, 255, self.coloreffect))
 	graphics.circle('fill', self.position[1] + self.irisPosition[1], self.position[2] + self.irisPosition[2], self.irisSize)
 	graphics.setColor(0, 0, 0)
 	graphics.circle('fill', self.position[1] + self.irisPosition[1], self.position[2] + self.irisPosition[2], self.pupilSize.var)
+	graphics.setPixelEffect()
 end
 
 function bossFive:handleDelete()
