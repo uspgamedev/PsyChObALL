83a83ba4d06b6e1ca0510f5862c03d27dd6bcf62
Rilifon <pudimdefrutas@gmail.com>
2014-01-20 01:34:11 -0200

    TEXT FILES + LEVEL 4-3
    Adicionei varias coisas no todo, arrumei peqeunas coisas no readme e
    changelog, e melhorei o 4-3. Tecnicamente ta terminado, mas quem sabe eu
    dou um ajustes finais depois, juntos com todos os outros levels.

--- game/levels/Level 4-3.lua
+++ game/levels/Level 4-3.lua
@@ -87,8 +87,8 @@ function run()
 	local vc = function(data) return Vector:new(data) end
 
 	wait(3)
-	enemy(sek, 1, nil, 7, Vector:new{width/2, -30})
-	wait(8)
+	enemy(sek, 1, nil, 5.3, Vector:new{width/2, -30})
+	wait(5.8)
 	enemy(sek, 1, { position = Vector:new{-30, height/2} }, 15, Vector:new{width/2, -30})
 	enemy(sek, 1, { position = Vector:new{width+30, height/2} }, 15, Vector:new{width/2, height-30})
 	enemy(sek, 1, { position = Vector:new{width/2, height+30} }, 15, Vector:new{width + 30, height/2})
@@ -116,28 +116,34 @@ function run()
 	wait(10)
 	enemy(sek, 1, nil, 18, Vector:new{width/2, -30})
 	enemy(sek, 1, nil, 20, Vector:new{width/2, height-30})
-	wait(3.5)
+	wait(1.5)
 	enemy(nic, 1, nil, {width/2,height/2},
-		{size = 180, wait = 25, sizeGrowth = 300},
+		{size = 180, wait = 22, sizeGrowth = 300},
 		{size = 300, wait = 15, sizeGrowth = 300},
 		{size = 100, wait = 5, sizeGrowth = 32},
 		{destroy = true, sizeGrowth = 460}
 	)
-	wait(10)
+	wait(5)
+	enemy(sek, 1, nil, 15.5, Vector:new{-30, height/2})
+	enemy(sek, 1, nil, 15.5, Vector:new{width+30, height/2})
+	wait(5)
 	enemy(super, 1, { position = Vector:new{width/2, height+30}, speed = Vector:new{0.4*v, 0.75*v}, life = 140, size = 60}, mono, {width/2, height+30}, 19)
-	wait(6)
+	wait(4)
 	for i = 1,3 do
 		enemy(sek, 1, { speed = Vector:new{(0.8-0.1*i)*v - 30,(0.8-0.1*i)*v}}, 11-2*i, Vector:new{width/2, height-30})
 		wait(2.5)
 	end
-	wait(7)
+	wait(2.5)
 	enemy(sek, 1, { position = Vector:new{-30, 2*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 13, Vector:new{-30, 2*height/5})
-	enemy(sek, 1, { position = Vector:new{-30, 3*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 10, Vector:new{-30, 3*height/5})
 	enemy(sek, 1, { position = Vector:new{-30, 4*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 10, Vector:new{-30, 4*height/5})
 	enemy(sek, 1, { position = Vector:new{width+30, 2*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 7, Vector:new{width+30, 2*height/5})
-	enemy(sek, 1, { position = Vector:new{width+30, 3*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 7, Vector:new{width+30, 3*height/5})
 	enemy(sek, 1, { position = Vector:new{width+30, 4*height/5}, speed = Vector:new{0.3*v, 0.3*v} }, 7, Vector:new{width+30, 4*height/5})
-	wait(19)
+	wait(4)
+	enemy(range, 1, nil, 6, vc{width/2, 45}, vc{width/2, -30}, nil, mono, {50,205,50}, 0, 15)
+	enemy(range, 1, nil, 6, vc{width/2, height - 45}, vc{width/2, height + 30}, nil, mono, {50,205,50}, 0, 15)
+	enemy(range, 1, nil, 4, vc{45, height/2}, vc{-30, height/2}, nil, mono, {50,205,50}, 0, 15)
+	enemy(range, 1, nil, 4, vc{width -45, height/2}, vc{width + 30, height/2}, nil, mono, {50,205,50}, 0, 15)
+	wait(17)
 	doNow( function(timer)
 		if not Levels.currentLevel.wasSelected then
 			if not DeathManager.gameLost then reloadStory 'Level 4-4' end
